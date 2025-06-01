7671969d267f4a7ce3a4188fb26f40914c691635
SeulGi0117 <jusanha17@gmail.com>
2025-06-02 01:27:30 +0900

    Git Actions로 EC2 서버 연결 commit Test

--- services/cow_service.py
+++ services/cow_service.py
@@ -6,6 +6,7 @@ def get_cow_list():
         {"name": "보균이", "id": "cow2", "number": "1014", "sensor": "221105", "date": "2021-01-05", "status": "양호", "milk": "20L", "birthdate": "2000-10-14", "breed": "Holstein"},
         {"name": "민지", "id": "cow3", "number": "0102", "sensor": "221106", "date": "2022-01-02", "status": "양호", "milk": "10L", "birthdate": "2002-01-02", "breed": "Hanwoo"},
         {"name": "슬기", "id": "cow4", "number": "0117", "sensor": "221107", "date": "2023-01-06", "status": "양호", "milk": "6L", "birthdate": "2002-01-17", "breed": "Dairy"},
+        {"name": "두리", "id": "cow5", "number": "0825", "sensor": "221109", "date": "2025-06-02", "status": "양호", "milk": "0L", "birthdate": "2019-08-25", "breed": "Bichon Frise"}
         
     ]
 # {
